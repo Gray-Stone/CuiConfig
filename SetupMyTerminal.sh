8db1954f8ffaff3b5f1bc1a6a63df775549e4e21
leo gray <leogray@localhost.localdomain>
2019-12-03 02:51:14 -0500

    finishing up setupscript

--- SetupMyTerminal.sh
+++ SetupMyTerminal.sh
@@ -1,3 +1,6 @@
+echo "installing oh-my-zsh with plugins"
+echo "   - git \n   - syntax-highlighting \n   - autosuggestions \n   - z"
+
 # install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
@@ -13,12 +16,17 @@ cp .zshrc ~/.zshrc
 # for parts that needs to be done at home directory
 cd 
 
+echo "installing additional plugins: \n   - trash-cli \n   - theFuck (with python3 and pip)"
 # install trash-cli 
 git clone https://github.com/andreafrancia/trash-cli.git
 cd trash-cli
 sudo python setup.py install
 
+#install python-3 for theFukc
+sudo apt install python3-dev python3-pip python3-setuptools
 
 #install theFuck
-sudo apt install python3-dev python3-pip python3-setuptools
 sudo pip3 install thefuck
+
+echo "switch to zsh"
+zsh
