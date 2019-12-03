22a6cb77224b85912f6df4caf2d65de4ebdfa8c9
leo gray <leogray@localhost.localdomain>
2019-12-03 03:36:41 -0500

    fixing directiry jump in installs

--- SetupMyTerminal.sh
+++ SetupMyTerminal.sh
@@ -1,20 +1,28 @@
 echo "installing oh-my-zsh with plugins"
-echo "   - git \n   - syntax-highlighting \n   - autosuggestions \n   - z"
+echo "   - git \n   - syntax-highlighting \n   - autosuggestions \n   - z \n \n \n \n \n "
 
 # install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
+echo "\n\n"
+
 # install zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+echo "\n\n"
 
 # install zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 
+echo "\n\n"
+
 cp .zshrc ~/.zshrc
 
 # for parts that needs to be done at home directory
 cd 
+
+echo "\n\n"
 
 echo "installing additional plugins: \n   - trash-cli \n   - theFuck (with python3 and pip)"
 # install trash-cli 
@@ -22,11 +30,18 @@ git clone https://github.com/andreafrancia/trash-cli.git
 cd trash-cli
 python setup.py install
 
+cd
+
+echo "\n\n"
+
 #install python-3 for theFukc
 apt install python3-dev python3-pip python3-setuptools
+
+echo "\n\n"
 
 #install theFuck
 pip3 install thefuck
 
-echo "switch to zsh"
-zsh
+echo "\n \n \n switch to zsh"
+echo "who am i? "
+whoami
--- readme.md
+++ readme.md
@@ -2,5 +2,6 @@ Document files and links for setting up a CUI with
 	ZSH and some plugins
 
 git clone https://github.com/Gray-Stone/zshCUIconfig.git
-sudo chomd +x SetupMyTerminal.sh  
+cd zshCUIconfig
+sudo chmod +x SetupMyTerminal.sh  
 sudo ./SetupMyTerminal.sh
